40a914e997b078bddec9a7abea980e4fdd1c9616
HiikiToSS <101421494+HiikiToSS@users.noreply.github.com>
2023-03-12 18:29:32 +0300

    brbrbr

--- aFlask Mac/mainBack.py
+++ aFlask Mac/mainBack.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 menu = [
     {'name' : 'О сайте', 'url' : 'about'},
     {'name' : 'Обратная связь', 'url' : 'contact'},
